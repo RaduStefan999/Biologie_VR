cddbd8235b727a27bfa64c681fd2b41c608616b8
raducu999 <radustefan1302@gmail.com>
2020-09-06 14:48:15 +0300

    hands

--- Assets/Scripts/HandPresence.cs
+++ Assets/Scripts/HandPresence.cs
@@ -5,16 +5,49 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
+    
+    private InputDevice targetDevice;
+    private GameObject spawnedHandModel;
+
     // Start is called before the first frame update
     void Start()
     {
+        TryInitialize();
+    }
+
+    void TryInitialize () {
+
         List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0) {
+            targetDevice = devices[0];
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            Debug.Log(spawnedHandModel.transform.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!targetDevice.isValid) {
+            TryInitialize();
+        }
+
         
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue) {
+            //Debug.Log("Primary button pressed");
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f) {
+            //Debug.Log("trigger pressed" + triggerValue);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero) {
+            //Debug.Log("Primary touchpad" + primary2DAxisValue);
+        }
     }
 }
