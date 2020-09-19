cc5d279fdc63b8e897260266c254b465cb94d262
raducu999 <radustefan1302@gmail.com>
2020-09-20 01:14:46 +0300

    data

---
+++ Assets/Scripts/PlantElement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class PlantElement
+{
+    public int id;
+    public string Name;
+    public Texture Image;
+    public string Information;
+}
