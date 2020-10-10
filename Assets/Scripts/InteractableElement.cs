716a970ed7cf8db4753fbd8ec1f75b023af67cc3
raducu999 <radustefan1302@gmail.com>
2020-10-10 23:52:57 +0300

    iteraction start

---
+++ Assets/Scripts/InteractableElement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class InteractableElement 
+{
+    public GameObject element;
+    public Material normalMaterial;
+    public Material highLightMaterial;
+    public GameObject descrition;
+
+    public bool highLighted = false;
+    public bool selected = false;
+
+
+    public void select () {
+
+    }
+
+    public void highLight () {
+
+    }
+
+    public void removeHighLight () {
+        
+    }
+}
